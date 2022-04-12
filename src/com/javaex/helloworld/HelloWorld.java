bdd29d66bf683003cb5386469003fcbd4c4ee725
joyunju <whdbswn1113@naver.com>
2022-04-13 01:20:07 +0900

    test

--- src/com/javaex/helloworld/HelloWorld.java
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -1,10 +1,12 @@
 //run 단축키 : ctrl + f11
+// 
 
 //패키지경로, 패키지명 : 패키지경로및 패키지명은 소문자로 작성
 package com.javaex.helloworld;
-
+// 클래스 영역 : 클래스명의 첫글자는 대문자로 작성 : 소스코드 파일의 이름과 클래스 이름은 동
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
+		
 	}
 }
