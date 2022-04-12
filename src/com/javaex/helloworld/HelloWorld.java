07400ae9fea3edfe6c202d15214afce5f0c400d7
joyunju <whdbswn1113@naver.com>
2022-04-12 17:52:29 +0900

    2022-04-12-01

---
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+//run 단축키 : ctrl + f11
+
+//패키지경로, 패키지명 : 패키지경로및 패키지명은 소문자로 작성
+package com.javaex.helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+}
